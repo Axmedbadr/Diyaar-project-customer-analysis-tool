64f5bee4fa4a9975dce0bc859c6dca6a3c541338
axmed badri <badri736@hotmail.com>
2026-02-24 12:08:42 -0800

    my first setup db and server

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async ()=>{
+   try{
+
+ await mongoose.connect(process.env.MONGO_URL);
+ console.log("mongodb Connected");
+
+
+   }
+   catch(error){
+    console.error(`there is problem of connectinf db `,error)
+    console.log("there is proble of connecting db ")
+
+   }
+};
+
+
+module.exports=connectDB
